234ca7d74476b1614fabc4e4e09846cf16a2fa54
Thiago <thiagodelgado.dev@gmail.com>
2023-07-17 14:21:39 -0300

    añado seeder usuario

---
+++ src/database/seeders/20230321150810-usuarios.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+
+const bcryptjs = require('bcryptjs')
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name : "Admin",
+          surname : "Test",
+          email : "admin@test.com",
+          pass : bcryptjs.hashSync('123123',10),
+          image: null,
+          rolId : 1,
+          addressId : 1,
+          createdAt : new Date(),
+          updatedAt : new Date(),
+        },
+        {
+          name : "User",
+          surname : "Test",
+          email : "user@test.com",
+          pass : bcryptjs.hashSync('123123',10),
+          image: null,
+          rolId : 2,
+          addressId : 2,
+          createdAt : new Date(),
+          updatedAt : new Date(),
+        },
+      
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
