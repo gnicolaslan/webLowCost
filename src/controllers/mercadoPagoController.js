5dcc07d546411c0ae3b4c0b579cd6d03f7d7a45d
gnicolaslan <gabrielnicolas.dg@gmail.com>
2023-09-18 02:51:37 -0300

    mercadopago estable

--- src/controllers/mercadoPagoController.js
+++ src/controllers/mercadoPagoController.js
@@ -1,7 +1,64 @@
 const express = require('express');
 const mercadopago = require('mercadopago');
+const transporter = require("../helpers/mailer");
 require('dotenv').config()
 
+const sendEmailsOnPurchase = async (values, cartItems) => {
+    const userEmail = values.email;
+    const sellerEmail = process.env.OWNER_EMAIL;
+
+    const cartDetails = cartItems.map((item) => {
+        return `
+        Producto: ${item.name}
+        Precio: ${item.price}
+        Cantidad: ${item.quantity}
+      `;
+    }).join("\n\n");
+
+    if (!userEmail || !sellerEmail) {
+        throw new Error("No se ha definido el destinatario del correo electrónico");
+    }
+
+    const mailOptionsUser = {
+        from: `LowCost ${process.env.EMAIL}`,
+        to: userEmail,
+        subject: "Gracias por comprar en LowCost Web",
+        html: `
+        <p>Hola ${values.name},</p>
+        <p>Gracias por comprar en nuestro sitio. Aquí están los detalles de tu compra:</p>
+        <pre>${cartDetails}</pre>
+        <p>Esperamos que disfrutes tus productos.<br>
+        Nuestro equipo de ventas se pondrá en contacto contigo para coordinar la logística del envío.</p>
+      `,
+    };
+
+    await transporter.sendMail(mailOptionsUser);
+
+    const mailOptionsSeller = {
+        from: `LowCost ${process.env.EMAIL}`,
+        to: sellerEmail,
+        subject: "Nueva Compra Realizada en LowCost Web",
+        html: `
+        <p>Hola Vendedor,</p>
+        <p>Se ha realizado una nueva compra en tu tienda en línea. Aquí están los detalles:</p>
+        <p>Detalles del Comprador:</p>
+        <pre>
+          Nombre: ${values.name}
+          Apellido: ${values.surname}
+          Email: ${values.email}
+          Teléfono: ${values.phone}
+          DNI: ${values.dni}
+          Dirección: ${values.street} ${values.streetNumber}, ${values.postCode}
+        </pre>
+        <p>Detalles de la Compra:</p>
+        <pre>${cartDetails}</pre>
+      `,
+    };
+
+    await transporter.sendMail(mailOptionsSeller);
+};
+
+let userDataAndCart = null;
 
 module.exports = {
 
@@ -10,7 +67,9 @@ module.exports = {
             access_token: "TEST-2938245235269496-091414-6edd5c1f1294b94467a89c6b720d2d7f-1479550679"
         });
 
-        const { description, price, quantity } = req.body;
+        const { description, price, quantity, values, cartItems } = req.body;
+        console.log("Datos recibidos del cuerpo:", req.body);
+        console.log('datos de usuario y compra', values, cartItems)
 
         try {
             const result = await mercadopago.preferences.create({
@@ -20,16 +79,16 @@ module.exports = {
                         unit_price: Number(price),
                         quantity: Number(quantity),
                         currency_id: "ARS"
-                    }
+                    },
                 ],
                 back_urls: {
-                    "success": "http://127.0.0.1:5173/purchase-accepted",
+                    "success": "http://localhost:3000/mp/success",
                     "failure": "http://127.0.0.1:5173/purchase-denied",
-                    "pending": "http://127.0.0.1:5173"
+                    "pending": "http://localhost:3000/pending"
                 },
                 auto_return: "approved",
             });
-            console.log(result)
+            console.log("Respuesta de MercadoPago:", result);
 
             res.json(result.body)
         } catch (error) {
@@ -37,18 +96,38 @@ module.exports = {
         }
     },
 
-    receiveWebhook: async (req, res) => {
+    buyerInfo: async (req, res) => {
+        const { values, cartItems } = req.body;
+
+        userDataAndCart = { values, cartItems };
+
+        console.log('Datos recibidos en el servidor:', userDataAndCart);
+    },
+
+    buySuccess: async (req, res) => {
         try {
-            const payment = req.query
-            console.log(payment)
-            if (payment.type === 'payment') {
-                const data = await mercadopago.payment.findById(payment["data.id"])
-                console.log(data)
+            const { collection_status, status } = req.query;
+
+            if (status === 'approved' || collection_status === 'approved') {
+                if (userDataAndCart) {
+                    const { values, cartItems } = userDataAndCart;
+
+                    await sendEmailsOnPurchase(values, cartItems);
+
+                    res.redirect("http://127.0.0.1:5173/purchase-accepted");
+                } else {
+                    return res.status(400).json({
+                        ok: false,
+                        error: "Datos de usuario o carrito faltantes",
+                    });
+                }
+            } else {
+                return res.redirect("http://127.0.0.1:5173/purchase-denied");
             }
-            res.sendStatus(204)
         } catch (error) {
-            console.log(error)
-            return res.status(500).json({ message: 'Something goes wrong' })
+            console.log(error);
+            return res.status(500).json({ message: 'Something goes wrong' });
         }
     }
+
 }
--- src/routes/mercadopago.js
+++ src/routes/mercadopago.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { createPreference, receiveWebhook } = require("../controllers/mercadoPagoController")
+const { createPreference, buyerInfo, buySuccess } = require("../controllers/mercadoPagoController")
 const router = express.Router();
 
 /* /mp */
 
 router.post("/create_preference", createPreference);
-router.post("/webhook", receiveWebhook);
-router.get("/success", (req, res) => res.send("Success"));
+router.post("/buyer_info", buyerInfo)
+router.get("/success", buySuccess);
 
 module.exports = router;
