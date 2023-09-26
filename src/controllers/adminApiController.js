236f700ebac3874ede94f5da871933e829670af2
gnicolaslan <gabrielnicolas.dg@gmail.com>
2023-09-26 13:28:00 -0300

    fixes

--- src/controllers/adminApiController.js
+++ src/controllers/adminApiController.js
@@ -36,37 +36,37 @@ module.exports = {
     },
     createProduct: async (req, res) => {
         try {
-        /*   if (!req.files || !req.files.imageFile) {
-            return res.status(400).send('No se han subido archivos.');
-          } */
-      
-          const { body } = req;
-      
-          // Convierte 'offer' y 'visible' a booleanos
-          body.offer = body.offer === 'on';
-          body.visible = body.visible === 'on';
-      
-          const newProduct = await createNewProduct(req.body,req.files);
-      
-          if (newProduct) {
-            return res.status(200).json({
-              ok: true,
-              data: newProduct,
-              meta: {
-                status: 200,
-                url: `/api/product/detail/${newProduct.id}`,
-              },
-            });
-          } else {
-            throw {
-              status: 500,
-              message: "Error al crear el producto",
-            };
-          }
+            /*             if (!req.files || !req.files.imageFile) {
+                            return res.status(400).send('No se han subido archivos.');
+                        } */
+
+            const { body } = req;
+
+            // Convierte 'offer' y 'visible' a booleanos
+            body.offer = body.offer === true;
+            body.visible = body.visible === true;
+            console.log(req.body)
+            const newProduct = await createNewProduct(req.body, req.files);
+
+            if (newProduct) {
+                return res.status(200).json({
+                    ok: true,
+                    data: newProduct,
+                    meta: {
+                        status: 200,
+                        url: `/api/product/detail/${newProduct.id}`,
+                    },
+                });
+            } else {
+                throw {
+                    status: 500,
+                    message: "Error al crear el producto",
+                };
+            }
         } catch (error) {
-          return createResponseError(res, error);
+            return createResponseError(res, error);
         }
-      },      
+    },
     deleteProduct: async (req, res) => {
         try {
             const { id } = req.params
