3647c1a301ad58fc7309c9de93264191588939a1
Thiago <thiagodelgado.dev@gmail.com>
2023-09-29 10:52:46 -0300

    add edit product price

--- src/controllers/adminApiController.js
+++ src/controllers/adminApiController.js
@@ -1,126 +1,173 @@
-const createResponseError = require('../helpers/createResponseError');
-const { getAllUsers, getAllProducts, createNewProduct, deleteProduct, productToEdit, editProduct } = require('../services/adminServices');
+const db = require("../database/models");
+const createResponseError = require("../helpers/createResponseError");
+const {
+  getAllUsers,
+  getAllProducts,
+  createNewProduct,
+  deleteProduct,
+  productToEdit,
+  editProduct,
+} = require("../services/adminServices");
+const { Op } = require('sequelize');
+
+
 
 module.exports = {
-    showListUsers: async (req, res) => {
-        try {
-            const users = await getAllUsers();
+  showListUsers: async (req, res) => {
+    try {
+      const users = await getAllUsers();
 
-            return res.status(200).json({
-                ok: true,
-                data: users,
-                meta: {
-                    status: 200,
-                    total: users.length,
-                }
-            })
-        } catch (error) {
-            return createResponseError(res, error)
-        }
-    },
-    showListProducts: async (req, res) => {
-        try {
-            const products = await getAllProducts();
+      return res.status(200).json({
+        ok: true,
+        data: users,
+        meta: {
+          status: 200,
+          total: users.length,
+        },
+      });
+    } catch (error) {
+      return createResponseError(res, error);
+    }
+  },
+  showListProducts: async (req, res) => {
+    try {
+      const products = await getAllProducts();
 
-            return res.status(200).json({
-                ok: true,
-                data: products,
-                meta: {
-                    status: 200,
-                    total: products.length,
-                }
-            })
-        } catch (error) {
-            return createResponseError(res, error)
-        }
-    },
-    createProduct: async (req, res) => {
-        try {
-            /*             if (!req.files || !req.files.imageFile) {
+      return res.status(200).json({
+        ok: true,
+        data: products,
+        meta: {
+          status: 200,
+          total: products.length,
+        },
+      });
+    } catch (error) {
+      return createResponseError(res, error);
+    }
+  },
+  createProduct: async (req, res) => {
+    try {
+      /*             if (!req.files || !req.files.imageFile) {
                             return res.status(400).send('No se han subido archivos.');
                         } */
 
-            const { body } = req;
+      const { body } = req;
 
-            // Convierte 'offer' y 'visible' a booleanos
-            body.offer = body.offer === true;
-            body.visible = body.visible === true;
-            const newProduct = await createNewProduct(req.body, req.files);
+      // Convierte 'offer' y 'visible' a booleanos
+      body.offer = body.offer === true;
+      body.visible = body.visible === true;
+      const newProduct = await createNewProduct(req.body, req.files);
 
-            if (newProduct) {
-                return res.status(200).json({
-                    ok: true,
-                    data: newProduct,
-                    meta: {
-                        status: 200,
-                        url: `/api/product/detail/${newProduct.id}`,
-                    },
-                });
-            } else {
-                throw {
-                    status: 500,
-                    message: "Error al crear el producto",
-                };
-            }
-        } catch (error) {
-            return createResponseError(res, error);
+      if (newProduct) {
+        return res.status(200).json({
+          ok: true,
+          data: newProduct,
+          meta: {
+            status: 200,
+            url: `/api/product/detail/${newProduct.id}`,
+          },
+        });
+      } else {
+        throw {
+          status: 500,
+          message: "Error al crear el producto",
+        };
+      }
+    } catch (error) {
+      return createResponseError(res, error);
+    }
+  },
+  deleteProduct: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedProduct = await deleteProduct(id);
+
+      return res.status(200).json({
+        ok: true,
+        message: "Product deleted with sucess",
+        meta: {
+          status: 200,
+          total: 1,
+        },
+        data: {
+          deletedProduct,
+        },
+      });
+    } catch (error) {
+      return createResponseError(res, error);
+    }
+  },
+  getEditProduct: async (req, res) => {
+    try {
+      const product = await productToEdit(req.params.id);
+
+      return res.status(200).json({
+        ok: true,
+        data: product,
+        meta: {
+          status: 200,
+          total: 1,
+        },
+      });
+    } catch (error) {
+      return createResponseError(res, error);
+    }
+  },
+  saveEditProduct: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const saveProduct = await editProduct(req.body, id);
+
+      return res.status(200).json({
+        ok: true,
+        meta: {
+          status: 200,
+          total: 1,
+          url: `/api/products/${id}`,
+        },
+        data: {
+          editedProduct: saveProduct,
+        },
+      });
+    } catch (error) {
+      return createResponseError(res, error);
+    }
+  },
+  editProductPrice: async (req, res) => {
+    try {
+      const { startId, endId, newPrice } = req.body;
+
+      const parsedStartId = parseInt(startId);
+      const parsedEndId = parseInt(endId);
+      const parsedNewPrice = parseInt(newPrice); 
+
+      if (isNaN(parsedStartId) || isNaN(parsedEndId) || isNaN(parsedNewPrice)) {
+        throw {
+          status: 400,
+          message: "startId, endId, and newPrice must be valid numbers.",
+        };
+      }
+      console.log(parsedStartId,parsedEndId,parsedNewPrice);
+
+      const updatedProducts = await db.Product.update(
+        { price: parsedNewPrice },
+        {
+          where: {
+            id: { [Op.between]: [parsedStartId, parsedEndId] },
+        },
         }
-    },
-    deleteProduct: async (req, res) => {
-        try {
-            const { id } = req.params
-            const deletedProduct = await deleteProduct(id);
+      );
 
-            return res.status(200).json({
-                ok: true,
-                message: 'Product deleted with sucess',
-                meta: {
-                    status: 200,
-                    total: 1
-                },
-                data: {
-                    deletedProduct
-                }
-            })
-
-        } catch (error) {
-            return createResponseError(res, error)
-        }
-    },
-    getEditProduct: async (req, res) => {
-        try {
-            const product = await productToEdit(req.params.id)
-
-            return res.status(200).json({
-                ok: true,
-                data: product,
-                meta: {
-                    status: 200,
-                    total: 1
-                }
-            })
-        } catch (error) {
-            return createResponseError(res, error)
-        }
-    },
-    saveEditProduct: async (req, res) => {
-        try {
-            const id = req.params.id;
-            const saveProduct = await editProduct(req.body, id)
-
-            return res.status(200).json({
-                ok: true,
-                meta: {
-                    status: 200,
-                    total: 1,
-                    url: `/api/products/${id}`
-                },
-                data: {
-                    editedProduct: saveProduct,
-                }
-            })
-        } catch (error) {
-            return createResponseError(res, error)
-        }
-    },
-}
+      return res.status(200).json({
+        ok: true,
+        message: "Prices updated successfully.",
+        meta: {
+          status: 200,
+          total: updatedProducts,
+        },
+      });
+    } catch (error) {
+      return createResponseError(res, error);
+    }
+  },
+};
--- src/routes/adminApi.js
+++ src/routes/adminApi.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const multer = require('multer');
-const { showListUsers, showListProducts, createProduct, deleteProduct, getEditProduct, saveEditProduct } = require('../controllers/adminApiController');
+const { showListUsers, showListProducts, createProduct, deleteProduct, getEditProduct, saveEditProduct, editProductPrice } = require('../controllers/adminApiController');
 var router = express.Router();
 
 const upload = multer({ dest: './public/uploads/' })
@@ -10,6 +10,7 @@ router
     .get('/users', showListUsers)
     .get('/products', showListProducts)
     .post('/create', upload.array('imageFiles', 3), createProduct)
+    .put('/edit/product-prices',editProductPrice)
     .delete('/:id', deleteProduct)
     .get('/edit/:id', getEditProduct)
     .put('/edit/:id', saveEditProduct)
