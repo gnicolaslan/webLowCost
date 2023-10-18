3469090b1f4fbcc6d6be15b5dd567be77ba39068
gnicolaslan <gabrielnicolas.dg@gmail.com>
2023-10-17 23:00:26 -0300

    add categories & brands seeders

--- src/database/seeders/20230718035708-categories.js
+++ src/database/seeders/20230718035708-categories.js
@@ -5,102 +5,46 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Categories',
       [
-        {
-          name: "Refrigeradores",
-          image: 'Refrigeradores.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Lavadoras",
-          image: 'Lavadoras.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Televisores",
-          image: 'Televisores.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Aires Acondicionados",
-          image: 'Aires Acondicionados.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Licuadoras",
-          image: 'Licuadoras.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Microondas",
-          image: 'Microondas.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Aspiradoras",
-          image: 'Aspiradoras.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Cafeteras",
-          image: 'Cafeteras.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Tostadoras",
-          image: 'Tostadoras.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Hornos",
-          image: 'Hornos.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Estufas",
-          image: 'Estufas.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Batidoras",
-          image: 'Batidoras.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Planchas",
-          image: 'Planchas.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Secadoras",
-          image: 'Secadoras.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "Cocinas",
-          image: 'Cocinas.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          name: "otro",
-          image: 'general.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
+        { name: "Art. de Baño", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Art. de Cocina", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Aires Acondicionados", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Aspiradoras", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Batidoras", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Bazar", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Bicicletas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Cafeteras", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Calefactores", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Caloventores", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Cocinas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Consolas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Celulares", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Estufas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Freezers", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Herramientas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Heladeras", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Hornos", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Juguetes", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Lavarropas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Lavavajillas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Licuadoras", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Mochilas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Microondas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Muebles", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Parlantes", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Pavas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Peluquería", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Piletas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Planchas", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "SmartWatchs", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Soportes de tv", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Tablets", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Tecno", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Televisores", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Termos", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Termotanques", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Tostadoras", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Ventiladores", image: '', createdAt: new Date(), updatedAt: new Date() },
+        { name: "Otros", image: 'general.png', createdAt: new Date(), updatedAt: new Date() },
       ],
       {});
 
